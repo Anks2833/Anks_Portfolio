e659457641545120230249eb1aa8b7546a1ccf5a
Anks2833 <dubeyankur2833@gmail.com>
2024-11-10 16:16:13 +0530

    fifteenth-commit

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useRef } from "react";
 import AnimatedMarquee from "../components/AnimatedMarquee"
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useGLTF, useAnimations  } from '@react-three/drei';
+import * as THREE from 'three';
 
 
 const Page2 = () => {
+
+    const Model = () => {
+        const actionRef = useRef();
+        const { scene, animations } = useGLTF('../../Models/Man.glb');
+        const { actions } = useAnimations(animations, scene);
+
+        // To know animation name
+        useEffect(() => {
+            if (animations) {
+                animations.forEach((animation, index) => {
+                    console.log(`Animation ${index}: ${animation.name}`);
+                });
+            }
+        }, [animations]);
+    
+        
+        useEffect(() => {
+            if (actions && actions['Armature|mixamo.com|Layer0']) {
+                actionRef.current = actions['Armature|mixamo.com|Layer0'];
+                actionRef.current.setLoop(THREE.LoopRepeat, Infinity);
+                actionRef.current.play();
+            }
+        }, [actions]);
+ 
+    
+        return (
+            <primitive
+                object={scene}
+                scale={[3.2, 3.2, 3.2]}
+                position={[-1, -3, 0]}
+                rotation={[0.3, 0.3, 0]}
+            />
+        );
+    };
+
     return (
         <div id="about" className='relative w-full h-screen bg-[#0B0D0C] text-white pt-12 flex flex-col items-center gap-16'>
 
@@ -18,6 +57,15 @@ const Page2 = () => {
                 <div className="absolute left-0 bottom-36">
                     <AnimatedMarquee />
                 </div>
+            </div>
+
+            <div className="absolute z-[100] w-[32vw] h-[25vw] right-20">
+                <Canvas className="w-full h-full">
+                    <ambientLight />
+                    <pointLight position={[10, 10, 10]} />
+                    <Model />
+                    {/* <OrbitControls /> */}
+                </Canvas>
             </div>
 
         </div>
